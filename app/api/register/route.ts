68c4f5178cfeca4d4171289e029542af2bf440f5
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-07-10 21:15:30 -0400

    fix(backend): :lock: improve security on register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,18 +4,43 @@ import prisma from '@/libs/prismadb'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-	const body = await request.json()
-	const { email, name, password } = body
+	try {
+		const body = await request.json()
+		const { email, name, password } = body
 
-	const hashedPassword = await bcrypt.hash(password, 12)
+		const hashedPassword = await bcrypt.hash(password, 12)
 
-	const user = await prisma.user.create({
-		data: {
-			email,
-			name,
-			hashedPassword,
-		},
-	})
+		if (!email || !name || !password) {
+			return new NextResponse('Missing Info!!!', { status: 400 })
+		}
 
-	return NextResponse.json(user)
+		if (password.length < 8) {
+			return new NextResponse(
+				'Password must have at least 8 characteres!!!',
+				{ status: 400 }
+			)
+		}
+
+		const checkUser = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		})
+		if (checkUser) {
+			return new NextResponse('User already exist!!!', { status: 400 })
+		}
+
+		const user = await prisma.user.create({
+			data: {
+				email,
+				name,
+				hashedPassword,
+			},
+		})
+
+		return NextResponse.json(user)
+	} catch (e) {
+		console.log('🚀 ~ file: route.ts:43 ~ POST ~ e:', e)
+		return new NextResponse('Internal Error!!!', { status: 500 })
+	}
 }
