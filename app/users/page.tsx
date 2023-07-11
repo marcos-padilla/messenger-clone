0b2c82b9f52804fe1a3385cbf53aa6426400857c
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-07-10 22:09:11 -0400

    feat(fronted): added empty state component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,9 @@
-'use client'
-import { signOut } from 'next-auth/react'
+import EmptyState from '@/components/EmptyState'
 
 export default function UsersPage() {
-	return <button onClick={() => signOut()}>logout</button>
+	return (
+		<div className='hidden lg:block lg:pl-80 h-full'>
+			<EmptyState />
+		</div>
+	)
 }
