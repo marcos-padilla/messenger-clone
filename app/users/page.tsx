37f5814fa6082b8f47da1bc965afa6fdc0b61c89
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-07-10 22:03:54 -0400

    feat: added a middleware for protect routes

---
+++ app/users/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { signOut } from 'next-auth/react'
+
+export default function UsersPage() {
+	return <button onClick={() => signOut()}>logout</button>
+}
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useForm, FieldValues, SubmitHandler } from 'react-hook-form'
 import Input from './inputs/Input'
 import Button from './Button'
@@ -8,14 +8,21 @@ import AuthSocialButton from './AuthSocialButton'
 import { BsGithub, BsGoogle } from 'react-icons/bs'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 type Variant = 'LOGIN' | 'REGISTER'
 
 export default function AuthForm() {
+	const session = useSession()
 	const [variant, setVariant] = useState<Variant>('LOGIN')
 	const [isLoading, setIsLoading] = useState(false)
 	const router = useRouter()
+
+	useEffect(() => {
+		if (session?.status === 'authenticated') {
+			router.push('/users')
+		}
+	}, [session?.status, router])
 
 	const toggleVariant = useCallback(() => {
 		setVariant((prev) => (prev === 'LOGIN' ? 'REGISTER' : 'LOGIN'))
@@ -27,8 +34,8 @@ export default function AuthForm() {
 		}
 
 		if (callback?.ok && !callback?.error) {
-			/* router.push('/conversations') */
 			toast.success('Logged in')
+			router.push('/users')
 		}
 	}, [])
 
@@ -79,6 +86,9 @@ export default function AuthForm() {
 			redirect: false,
 		})
 			.then(handleCallback)
+			.catch((e) => {
+				console.log({ e })
+			})
 			.finally(() => setIsLoading(false))
 	}
 
