b062779834cf2177fb592127daf28623d70a0f86
marcos-padilla <marcosalejandro.pd@gmail.com>
2023-07-11 10:13:12 -0400

    feat: passing current user to DesktopSidebar

--- components/sidebar/DesktopSidebar.tsx
+++ components/sidebar/DesktopSidebar.tsx
@@ -2,8 +2,14 @@
 import useRoutes from '@/hooks/useRoutes'
 import { useState } from 'react'
 import DesktopItem from './DesktopItem'
+import { User } from '@prisma/client'
+import Avatar from '../Avatar'
 
-export default function DesktopSidebar() {
+interface DesktopSidebarProps {
+	currentUser: User
+}
+
+export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
 	const routes = useRoutes()
 	const [isOpen, setIsOpen] = useState(false)
 
@@ -18,6 +24,14 @@ export default function DesktopSidebar() {
 						<DesktopItem key={item.label} {...item} />
 					))}
 				</ul>
+			</nav>
+			<nav className='mt-4 flex flex-col justify-between items-center'>
+				<div
+					onClick={() => setIsOpen(true)}
+					className='cursor-pointer hover:opacity-75 transition'
+				>
+					<Avatar user={currentUser} />
+				</div>
 			</nav>
 		</div>
 	)
